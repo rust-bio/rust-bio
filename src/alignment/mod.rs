3a16f4c57f2f2e2fd6130d4413b88d4a70c2a956
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-02-01 16:46:56 +0100

    Refactoring and cigar strings.

--- src/alignment/mod.rs
+++ src/alignment/mod.rs
@@ -21,3 +21,40 @@ pub struct Alignment {
     pub j: usize,
     pub operations: Vec<AlignmentOperation>
 }
+
+
+impl Alignment {
+    pub fn get_cigar(&self) -> String {
+        let add_op = |&: op, k, cigar: &mut String| {
+            cigar.push_str(format!("{}{}", k, match op {
+                AlignmentOperation::Match => "=",
+                AlignmentOperation::Subst => "X",
+                AlignmentOperation::Del => "D",
+                AlignmentOperation::Ins => "I",
+            }).as_slice());
+        };
+        let mut cigar = String::new();
+
+        if self.j > 0 {
+            cigar.push_str(format!("{}{}", self.j, "S").as_slice());
+        }
+
+        if !self.operations.is_empty() {
+            let mut last = self.operations[0];
+            let mut k = 1;
+            for &op in self.operations.iter() {
+                if op == last {
+                    k += 1;
+                } else {
+                    add_op(last, k, &mut cigar);
+                    k = 1;
+                }
+                last = op;
+            }
+        }
+
+        // TODO add end clipping
+
+        cigar
+    }
+}
