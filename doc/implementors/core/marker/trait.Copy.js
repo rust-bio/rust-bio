f023f5b8e260fe488609e5ed50472ca5dfa327d6
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-02-01 16:09:37 +0100

    Updated docs.

---
+++ doc/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bio'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='bio/pattern_matching/shift_and/struct.ShiftAnd.html' title='bio::pattern_matching::shift_and::ShiftAnd'>ShiftAnd</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='bio/pattern_matching/bndm/struct.BNDM.html' title='bio::pattern_matching::bndm::BNDM'>BNDM</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='bio/alignment/enum.AlignmentOperation.html' title='bio::alignment::AlignmentOperation'>AlignmentOperation</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
