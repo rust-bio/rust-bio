c145131cd3c0155be7e90c50c24ceb1963f4c1ae
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-06-27 21:57:22 -0400

    Improved numerical stability of comb

--- src/stats.rs
+++ src/stats.rs
@@ -12,14 +12,15 @@ use std::cmp;
 /// Calculate the number of combinations when choosing
 /// k elements from n elements without replacement.
 /// This is also known as n over k, or the binomial coefficient.
-pub fn comb(n: u64, k: u64) -> u64 {
+pub fn comb(n: u64, k: u64) -> f64 {
     if k > n {
-        0
+        0.0
     }
     else {
-        let mut comb = 1;
+        let mut comb = 1.0;
         for j in 0..cmp::min(k, n-k) {
-            comb = (comb * (n - j)) / (j + 1)
+            comb /= (j + 1) as f64;
+            comb *= (n - j) as f64;
         }
         comb
     }
@@ -28,7 +29,7 @@ pub fn comb(n: u64, k: u64) -> u64 {
 
 /// Calculate the number of combinations when choosing
 /// k elements from n elements with replacement.
-pub fn comb_with_repl(n: u64, k: u64) -> u64 {
+pub fn comb_with_repl(n: u64, k: u64) -> f64 {
     comb(n + k - 1, k)
 }
 
@@ -39,7 +40,8 @@ mod tests {
 
     #[test]
     fn test_comb() {
-        assert_eq!(comb(10, 3), 120);
-        assert_eq!(comb_with_repl(10, 3), 220);
+        assert_eq!(comb(10, 3), 120.0);
+        assert_eq!(comb_with_repl(10, 3), 220.0);
+        assert_eq!(comb(200, 10), 22451004309013280.0);
     }
 }
