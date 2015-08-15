1dbce55bae88e42e32a01860702cac59d3d77555
Vadim Nazarov <vdn@mailbox.org>
2015-08-15 18:54:32 +0300

    add distance.rs file

---
+++ src/alignment/distance.rs
@@ -0,0 +1,6 @@
+// Copyright 2015 Vadim Nazarov.
+// Licensed under the MIT license (http://opensource.org/licenses/MIT)
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Various subroutines for computing a distance between sequences.
