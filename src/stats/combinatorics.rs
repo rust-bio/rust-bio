0d1f280f694a9ca11345f742ba68d6cf6ac1a360
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-18 13:21:15 -0400

    Introduce scaled combinations.

--- src/stats/combinatorics.rs
+++ src/stats/combinatorics.rs
@@ -7,23 +7,31 @@
 
 
 use std::cmp;
+use std::f64;
 
 
 /// Calculate the number of combinations when choosing
-/// k elements from n elements without replacement.
-/// This is also known as n over k, or the binomial coefficient.
-pub fn combinations(n: u64, k: u64) -> f64 {
+/// k elements from n elements without replacement, multiplied by a scaling factor.
+pub fn scaled_combinations(n: u64, k: u64, scale: f64) -> f64 {
     if k > n {
         0.0
     }
     else {
-        let mut comb = 1.0;
+        let mut comb = scale;
         for j in 0..cmp::min(k, n-k) {
             comb /= (j + 1) as f64;
             comb *= (n - j) as f64;
         }
         comb
     }
+}
+
+
+/// Calculate the number of combinations when choosing
+/// k elements from n elements without replacement.
+/// This is also known as n over k, or the binomial coefficient.
+pub fn combinations(n: u64, k: u64) -> f64 {
+    scaled_combinations(n, k, 1.0)
 }
 
 
@@ -43,5 +51,10 @@ mod tests {
         assert_eq!(combinations(10, 3), 120.0);
         assert_eq!(combinations_with_repl(10, 3), 220.0);
         assert_eq!(combinations(200, 10), 22451004309013280.0);
+    }
+
+    #[test]
+    fn test_comb_scaled() {
+        assert!((scaled_combinations(150, 80, 1e-5) - 6.6643938163479384e+38).abs() < 0.0000001);
     }
 }
