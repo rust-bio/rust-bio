7b0b71439fff82816342a0c2030219a7be40c0df
llogiq <bogusandre@gmail.com>
2016-03-02 19:41:11 +0100

    Use type aliases for BWTSlice and SuffixSlice

--- src/data_structures/bwt.rs
+++ src/data_structures/bwt.rs
@@ -11,9 +11,10 @@ use std::iter::repeat;
 
 use utils::prescan;
 use alphabets::Alphabet;
-
+use data_structures::suffix_array::SuffixSlice;
 
 pub type BWT = Vec<u8>;
+pub type BWTSlice = [u8];
 pub type Less = Vec<usize>;
 pub type BWTFind = Vec<usize>;
 
@@ -36,7 +37,7 @@ pub type BWTFind = Vec<usize>;
 /// let bwt = bwt(text, &pos);
 /// assert_eq!(bwt, b"ATTATTCAGGACCC$CTTTCAA");
 /// ```
-pub fn bwt(text: &[u8], pos: &[usize]) -> BWT {
+pub fn bwt(text: &[u8], pos: &SuffixSlice) -> BWT {
     assert!(text.len() == pos.len());
     let n = text.len();
     let mut bwt: BWT = repeat(0).take(n).collect();
@@ -59,7 +60,7 @@ pub fn bwt(text: &[u8], pos: &[usize]) -> BWT {
 /// # Arguments
 ///
 /// * `bwt` - the BWT
-pub fn invert_bwt(bwt: &[u8]) -> Vec<u8> {
+pub fn invert_bwt(bwt: &BWTSlice) -> Vec<u8> {
     let alphabet = Alphabet::new(bwt);
     let n = bwt.len();
     let bwtfind = bwtfind(bwt, &alphabet);
@@ -95,7 +96,7 @@ impl Occ {
     ///
     /// * `bwt` - the BWT
     /// * `k` - the sampling rate: every k-th entry will be stored
-    pub fn new(bwt: &[u8], k: usize, alphabet: &Alphabet) -> Self {
+    pub fn new(bwt: &BWTSlice, k: usize, alphabet: &Alphabet) -> Self {
         let n = bwt.len();
         let m = alphabet.max_symbol().expect("Expecting non-empty alphabet.") as usize + 1;
         let mut occ = Vec::with_capacity(n / k);
@@ -112,7 +113,7 @@ impl Occ {
 
     /// Get occurrence count of symbol a in BWT[..r+1].
     /// Complexity: O(k).
-    pub fn get(&self, bwt: &[u8], r: usize, a: u8) -> usize {
+    pub fn get(&self, bwt: &BWTSlice, r: usize, a: u8) -> usize {
         let i = r / self.k;
         // TODO use sum() once it has been stabilized: .sum::<usize>()
         self.occ[i][a as usize] +
@@ -122,7 +123,7 @@ impl Occ {
 
 
 /// Calculate the less array for a given BWT. Complexity O(n).
-pub fn less(bwt: &[u8], alphabet: &Alphabet) -> Less {
+pub fn less(bwt: &BWTSlice, alphabet: &Alphabet) -> Less {
     let m = alphabet.max_symbol().expect("Expecting non-empty alphabet.") as usize + 2;
     let mut less: Less = repeat(0)
                              .take(m)
@@ -138,7 +139,7 @@ pub fn less(bwt: &[u8], alphabet: &Alphabet) -> Less {
 
 
 /// Calculate the bwtfind array needed for inverting the BWT. Complexity O(n).
-pub fn bwtfind(bwt: &[u8], alphabet: &Alphabet) -> BWTFind {
+pub fn bwtfind(bwt: &BWTSlice, alphabet: &Alphabet) -> BWTFind {
     let n = bwt.len();
     let mut less = less(bwt, alphabet);
 
--- src/data_structures/suffix_array.rs
+++ src/data_structures/suffix_array.rs
@@ -22,6 +22,7 @@ use data_structures::smallints::SmallInts;
 
 
 pub type SuffixArray = Vec<usize>;
+pub type SuffixSlice = [usize];
 pub type LCPArray = SmallInts<i8, isize>;
 
 /// Construct suffix array for given text of length n.
@@ -125,7 +126,7 @@ pub fn suffix_array(text: &[u8]) -> SuffixArray {
 ///     ]
 /// )
 /// ```
-pub fn lcp(text: &[u8], pos: &[usize]) -> LCPArray {
+pub fn lcp(text: &[u8], pos: &SuffixSlice) -> LCPArray {
     assert!(text.len() == pos.len());
     let n = text.len();
 
