14450ef518e6123363284fea2b7345c07518f9c2
althonos <althonos@users.noreply.github.com>
2016-03-27 23:01:23 +0200

    Corrected case of magic numbering

--- src/seq_analysis/gc.rs
+++ src/seq_analysis/gc.rs
@@ -7,30 +7,37 @@
 
 // Maybe not the most clever way to do it, but at least runs in o(n)
 
+const LOWER_G:u8 = 103;
+const LOWER_C:u8 = 99;
+
+const G:u8 = 71;
+const C:u8 = 67;
+
+
 pub fn gc <'a>(sequence: &'a [u8]) -> f32 {
-    let mut n: f32 = 0.0;
-    let l: usize = sequence.len();
+    let mut count: f32 = 0.0;
+    let len: usize = sequence.len();
     let mut i: usize = 0;
-    while i < l {
-        n += match sequence[i] {
-            67 | 71 => 1f32, // G or C
-            _       => 0f32,
+    while i < len {
+        count += match sequence[i] {
+            C|LOWER_C|G|LOWER_G => 1f32, // G or C
+                              _ => 0f32,
         } ;
         i += 1
     }
-    n / l as f32
+    count / len as f32
 }
 
 pub fn gc3 <'a>(sequence: &'a [u8]) -> f32 {
-    let mut n: f32 = 0.0;
-    let l: usize = sequence.len();
+    let mut count: f32 = 0.0;
+    let len: usize = sequence.len();
     let mut i: usize = 2;
-    while i < l {
-        n += match sequence[i] {
-            67 | 71 => 1f32,
-            _       => 0f32,
+    while i < len {
+        count += match sequence[i] {
+            C|LOWER_C|G|LOWER_G => 1f32,
+                              _ => 0f32,
         };
         i += 3;
     }
-    n / l as f32
+    count / len as f32
 }
