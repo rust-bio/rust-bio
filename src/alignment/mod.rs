0cb55c904aaf4f252646cb0fea5c535b25fb9362
Patrick Marks <patrick@10xgenomics.com>
2016-06-11 10:48:34 -0700

    add name to copyright

--- src/alignment/mod.rs
+++ src/alignment/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2014-2015 Johannes Köster, Vadim Nazarov.
+// Copyright 2014-2015 Johannes Köster, Vadim Nazarov, Patrick Marks
 // Licensed under the MIT license (http://opensource.org/licenses/MIT)
 // This file may not be copied, modified, or distributed
 // except according to those terms.
