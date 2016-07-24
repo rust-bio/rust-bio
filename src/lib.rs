1dfac5e82ad7657ac163cf915af176fd8da17946
Taylor Cramer <cramertj@cs.washington.edu>
2016-07-24 16:23:34 -0700

    Clarify doc examples and provide multithreaded example

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,6 @@
 //! use bio::data_structures::suffix_array::suffix_array;
 //! use bio::data_structures::bwt::{bwt, less, Occ};
 //! use bio::data_structures::fmindex::{FMIndex, FMIndexable};
-//! use std::rc::Rc;
 //!
 //! let text = b"ACGGATGCTGGATCGGATCGCGCTAGCTA$";
 //! let pattern = b"ACCG";
@@ -40,13 +39,48 @@
 //! // Create an FM-Index for a given text.
 //! let alphabet = alphabets::dna::iupac_alphabet();
 //! let sa = suffix_array(text);
-//! let bwt = Rc::new(bwt(text, &sa));
-//! let less = Rc::new(less(&bwt, &alphabet));
-//! let occ = Rc::new(Occ::new(&bwt, 3, &alphabet));
-//! let fmindex = FMIndex::new(bwt, less, occ);
+//! let bwt = bwt(text, &sa);
+//! let less = less(&bwt, &alphabet);
+//! let occ = Occ::new(&bwt, 3, &alphabet);
+//! let fmindex = FMIndex::new(&bwt, &less, &occ);
 //!
 //! let interval = fmindex.backward_search(pattern.iter());
 //! let positions = interval.occ(&sa);
+//! ```
+//!
+//! # Multithreaded Example
+//!
+//! ```rust
+//! use bio::alphabets;
+//! use bio::data_structures::suffix_array::suffix_array;
+//! use bio::data_structures::bwt::{bwt, less, Occ};
+//! use bio::data_structures::fmindex::{FMIndex, FMIndexable};
+//! use std::sync::Arc;
+//! use std::thread;
+//!
+//! let text = b"ACGGATGCTGGATCGGATCGCGCTAGCTA$";
+//! let patterns = vec![b"ACCG", b"TGCT"];
+//!
+//! // Create an FM-Index for a given text.
+//! let alphabet = alphabets::dna::iupac_alphabet();
+//! let sa = suffix_array(text);
+//! let bwt = Arc::new(bwt(text, &sa));
+//! let less = Arc::new(less(bwt.as_ref(), &alphabet));
+//! let occ = Arc::new(Occ::new(bwt.as_ref(), 3, &alphabet));
+//! let fmindex = Arc::new(FMIndex::new(bwt, less, occ));
+//!
+//! // Spawn threads to perform backward searches for each interval
+//! let interval_calculators = patterns.into_iter().map(|pattern| {
+//!     let fmindex = fmindex.clone();
+//!     thread::spawn(move ||
+//!         fmindex.backward_search(pattern.iter())
+//!     )
+//! }).collect::<Vec<_>>();
+//!
+//! // Loop through the results, extracting the positions array for each pattern
+//! for interval_calculator in interval_calculators {
+//!     let positions = interval_calculator.join().unwrap().occ(&sa);
+//! }
 //! ```
 //!
 //! Documentation and further examples for each module can be found in the module descriptions below.
