cf0cb8d5a042ef4817ced340cd342ddd1db6e39a
Patrick Marks <patrick@10xgenomics.com>
2016-11-30 10:14:07 -0800

    return a struct, add more docs

--- src/alignment/sparse.rs
+++ src/alignment/sparse.rs
@@ -22,24 +22,42 @@
 //! let s2 = "TTACGTACGATAGGTATT";
 //! let k = 8;
 //! let matches = find_kmer_matches(&s1, &s2, k);
-//! let (tb, score,  _) = lcskpp(&matches, k);
-//! let match_path: Vec<(u32,u32)> = tb.iter().map(|i| matches[*i]).collect();
+//! let sparse_al = lcskpp(&matches, k);
+//! let match_path: Vec<(u32,u32)> = sparse_al.path.iter().map(|i| matches[*i]).collect();
 //! assert_eq!(match_path, vec![(0,2), (1,3), (2,4), (3,5), (4,6), (5,7), (6,8)]);
-//! assert_eq!(score, 14);
+//! assert_eq!(sparse_al.score, 14);
 
 use std::cmp::max;
 use data_structures::bit_tree::MaxBitTree;
 use data_structures::qgram_index::QGramIndex;
 use alphabets::Alphabet;
 
+/// Result of a sparse alignment
+pub struct SparseAlignmentResult {
+    /// LCSk++ path, represented as vector of indices into the input matches vector.
+    pub path: Vec<usize>,
+    // Score of the path, which is the number of bases covered by the matched kmers.
+    pub score: u32,
+    // Full DP vector, which can generally be ignored. (It may be useful for testing purposes).
+    pub dp_vector: Vec<(u32, i32)>,
+}
+
 /// Sparse DP routine for Longest Common Subsequence in length k substrings.  Also known of LCSk++
 /// From LCSk++: Practical similarity metric for long strings. Filip Pavetić, Goran Žužić, Mile Šikić
 /// Paper here :https://arxiv.org/abs/1407.2407.  Original implementation here: https://github.com/fpavetic/lcskpp
-/// matches is a tuple of the positions of all k-mer matches between two strings. matches vector must be sorted by caller.
-/// The first element of the return tuple is the LCSk++ path, represented as vector of indices into the matches vector.
-/// The second element of the return tuple is the score of the path, which is the number of bases covered by the matched kmers.
-/// The third element of the return tuple is full DP vector, which can generally be ignored. (It may be useful for testing purposes).
-pub fn lcskpp(matches: &Vec<(u32, u32)>, k: usize) -> (Vec<usize>, u32, Vec<(u32,i32)>) {
+///
+/// # Arguments
+///
+/// * `matches` - a vector of tuples indicating the (string1 position, string2 position) kmer matches between the strings  
+/// * `k` - the kmer length used for matching
+///
+/// # Return value
+///
+/// The method returns a `SparseAlignmentResult` struct with the following fields:
+/// * `path` is the LCSk++ path, represented as vector of indices into the input matches vector.
+/// * `score` is the score of the path, which is the number of bases covered by the matched kmers.
+/// * `dp_vector` is the full DP vector, which can generally be ignored. (It may be useful for testing purposes).
+pub fn lcskpp(matches: &Vec<(u32, u32)>, k: usize) -> SparseAlignmentResult {
 
     let k = k as u32;
 
@@ -107,8 +125,7 @@ pub fn lcskpp(matches: &Vec<(u32, u32)>, k: usize) -> (Vec<usize>, u32, Vec<(u32
         prev_match = dp[prev_match as usize].1;
     }
     traceback.reverse();
-
-    (traceback, best_score, dp)
+    SparseAlignmentResult { path: traceback, score: best_score, dp_vector: dp }
 }
 
 
@@ -133,6 +150,10 @@ pub fn find_kmer_matches<T: AsRef<[u8]>>(query: &T, reference: &T, k: usize) -> 
     let qg_matches = qgram.exact_matches(slc1);
 
     let mut matches = Vec::new();
+
+    // Q-gram matches will be >= k in length, but each individual kmer match 
+    // must appear in mathces, so for a Q-gram match of length L, 
+    // generate L - k + 1 matches.hh
     for m in qg_matches {
         for offset in 0 .. m.pattern.stop - m.pattern.start - k + 1 {
             matches.push(((m.pattern.start + offset) as u32, (m.text.start + offset) as u32));
@@ -168,10 +189,10 @@ mod sparse_alignment {
         let s2 = "TTACGTACGATAGGTATT";
         let k = 8;
         let matches = super::find_kmer_matches(&s1, &s2, k);
-        let (tb, score,  _) = super::lcskpp(&matches, k);
-        let match_path: Vec<(u32,u32)> = tb.iter().map(|i| matches[*i]).collect();
+        let res = super::lcskpp(&matches, k);
+        let match_path: Vec<(u32,u32)> = res.path.iter().map(|i| matches[*i]).collect();
         assert_eq!(match_path, vec![(0,2), (1,3), (2,4), (3,5), (4,6), (5,7), (6,8)]);
-        assert_eq!(score, 14);
+        assert_eq!(res.score, 14);
      }
 
 
@@ -182,7 +203,7 @@ mod sparse_alignment {
         let k = 8;
 
         let matches = super::find_kmer_matches(&s1, &s2, k);
-        let (_, score, _) = super::lcskpp(&matches, k);
+        let res = super::lcskpp(&matches, k);
         
         // For debugging: 
         //for (idx, (ev, (score, prev))) in evs.iter().zip(dps.clone()).enumerate() {
@@ -191,7 +212,7 @@ mod sparse_alignment {
         //println!("tb: {:?}", tb);
 
         // Should have 25bp group of matches plus a 24 bp group of matches
-        assert_eq!(score, 25 + 24);
+        assert_eq!(res.score, 25 + 24);
      }
 
     #[test]
@@ -201,7 +222,7 @@ mod sparse_alignment {
         let k = 5;
 
         let matches = super::find_kmer_matches(&s1, &s1, k);
-        let (tb, score, _) = super::lcskpp(&matches, k);
+        let res = super::lcskpp(&matches, k);
 
         // For debugging: 
         //for (idx, (ev, (score, prev))) in evs.iter().zip(dps.clone()).enumerate() {
@@ -209,10 +230,10 @@ mod sparse_alignment {
         //}
         //println!("tb: {:?}", tb);
 
-        assert_eq!(score, s1.len() as u32);
+        assert_eq!(res.score, s1.len() as u32);
 
-        for i in 0..tb.len() {
-            assert_eq!(matches[tb[i] as usize], (i as u32, i as u32));
+        for i in 0..res.path.len() {
+            assert_eq!(matches[res.path[i] as usize], (i as u32, i as u32));
         }
      }
  }
