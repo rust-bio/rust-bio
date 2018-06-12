946ebbee8cecf41fe0bb3457e7d4ddafee1aa966
jeff-k <jeff@x-0.ca>
2018-06-12 09:44:21 -0700

    more test cases

--- src/pattern_matching/mating.rs
+++ src/pattern_matching/mating.rs
@@ -11,8 +11,8 @@
 //! use bio::pattern_matching::mating::{mate,merge};
 //! let read1 = b"tacgattcgat";
 //! let read2 = b"ttcgattacgt";
-//! let offset = mate(read1, read2, 0, 0).unwrap();
-//! let contig = merge(read1, read2, offset);
+//! let overlap = mate(read1, read2, 1, 1).unwrap();
+//! let contig = merge(read1, read2, overlap);
 //! assert_eq!(contig, b"tacgattcgattacgt");
 //! ```
 //!
@@ -30,8 +30,9 @@
 use std::cmp;
 
 // return the highest scoring overlap length if possible
-pub fn mate(r1: &[u8], r2: &[u8], min_overlap: usize, min_score: i16) -> Option<usize> {
+pub fn mate(r1: &[u8], r2: &[u8], overlap_bound: usize, min_score: i16) -> Option<usize> {
     let max_overlap = cmp::min(r1.len(), r2.len());
+    let min_overlap = overlap_bound - 1;
     let mut m: i16 = 0;
     let mut overlap: usize = 0;
     for i in min_overlap..max_overlap {
@@ -41,7 +42,7 @@ pub fn mate(r1: &[u8], r2: &[u8], min_overlap: usize, min_score: i16) -> Option<
             overlap = i;
         }
     }
-    if m > min_score {
+    if m >= min_score {
         return Some(overlap)
     }
     None
@@ -86,7 +87,6 @@ pub fn merge(r1: &[u8], r2: &[u8], overlap: usize) -> Vec<u8> {
     for i in 0..overlap {
         seq[r1_end + i] = mend_consensus(r1[r1_end + i], r2[i]);
     }
-
     seq[(r1_end + overlap)..len].copy_from_slice(&r2[overlap..r2.len()]);
     seq
 }
@@ -99,7 +99,7 @@ mod tests {
     fn test_mate_pair() {
         let r1 = b"tacgattcgat";
         let r2 =      b"ttcgattacgt";
-        let offset = mate(r1, r2, 0, 0).unwrap();
+        let offset = mate(r1, r2, 3, 3).unwrap();
         assert_eq!(offset, 6);
     }
 
@@ -114,8 +114,24 @@ mod tests {
     fn test_merge_consensus() {
         let r1 = b"actgtagtacaccatgatg";
         let r2 =       b"gtttaccatgatggattga";
-        let offset = mate(r1, r2, 0, 0).unwrap();
+        let offset = mate(r1, r2, 3, 3).unwrap();
         assert_eq!(offset, 13);  
         assert_eq!(merge(r1, r2, offset), b"actgtagtNNaccatgatggattga");
     }
+
+    #[test]
+    fn test_overlap_bounds() {
+        let r1 = b"ctagtcagctagcatgcatgctcgcgtatacgctagc";
+        let r2 =                          b"gtatacgctagccgctagcatgcat";
+        assert_eq!(mate(r1, r2, 12, 12).unwrap(), 12); 
+        assert_eq!(mate(r1, r2, 13, 13), None);
+    }
+
+    #[test]
+    fn test_score_bounds() {
+        let r1 = b"there is no restriction to DNA strings";
+        let r2 =                   b"ctiin ot dNA atrings asdf qwer";
+        assert_eq!(mate(r1, r2, 12, 10).unwrap(), 20); 
+        assert_eq!(mate(r1, r2, 12, 11), None);
+    } 
 }
