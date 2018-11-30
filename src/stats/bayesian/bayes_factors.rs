fa8a6873ef7ccee7590413011b9eb134204fef09
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-11-30 10:48:39 +0100

    Serde support for KassRaftery.

--- src/stats/bayesian/bayes_factors.rs
+++ src/stats/bayesian/bayes_factors.rs
@@ -3,7 +3,7 @@ use stats::LogProb;
 pub mod evidence {
     /// Scale of evidence as defined by
     /// [Kass and Raftery 1995](http://www.andrew.cmu.edu/user/kk3n/simplicity/KassRaftery1995.pdf).
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
     pub enum KassRaftery {
         None,
         Barely,
