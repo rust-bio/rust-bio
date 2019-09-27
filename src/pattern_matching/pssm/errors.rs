534fcc604855360614f0880b4f7695f849854597
Johannes Köster <johannes.koester@uni-due.de>
2019-09-27 08:59:56 +0200

    HMM error migration, prob error migration.

--- src/pattern_matching/pssm/errors.rs
+++ src/pattern_matching/pssm/errors.rs
@@ -1,3 +1,9 @@
+// Copyright 2019 Johannes Köster, University of Duisburg-Essen.
+// Licensed under the MIT license (http://opensource.org/licenses/MIT)
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Error definitions for the `pssm` module.
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
