ecacfc4474653411bd37068021bea40f62a03aec
Johannes Köster <johannes.koester@uni-due.de>
2020-03-06 10:55:23 +0100

    fix lints

--- src/utils/fastexp.rs
+++ src/utils/fastexp.rs
@@ -8,6 +8,7 @@
 //! `bio::stats::LogProb`.
 
 use num_traits::Float;
+use std::f64;
 use std::ops;
 
 const COEFF_0: f64 = 1.0;
@@ -15,7 +16,7 @@ const COEFF_1: f64 = 4.831_794_110;
 const COEFF_2: f64 = 0.143_440_676;
 const COEFF_3: f64 = 0.019_890_581;
 const COEFF_4: f64 = 0.006_935_931;
-const ONEBYLOG2: f64 = 1.442_695_041;
+const ONEBYLOG2: f64 = f64::consts::LOG2_E;
 const OFFSET_F64: i64 = 1023;
 const FRACTION_F64: u32 = 52;
 const MIN_VAL: f64 = -500.0;
