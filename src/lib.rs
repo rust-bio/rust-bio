d0631e9f89776fe4286e69f73c9586ba2b75081a
David Laehnemann <david.laehnemann@hhu.de>
2020-06-24 17:06:49 +0200

    Integrate essential info from website into README.md and rendered docs, to make the website obsolete.
    Also: Update FASTQ parsing examples to a pre-allocated record, which is a more efficient method.

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,9 @@
 //! that are useful for bioinformatics.
 //! All provided implementations are rigorously tested via continuous
 //! integration.
-//! For installation instructions and a general overview, visit
-//! https://rust-bio.github.io.
+//!
+//! For **getting started** with using `rust-bio`, see [the `Getting started` section below](#getting-started).
+//! For navigating the documentation of the available modules, see [the `Modules` section below](#modules).
 //!
 //! Currently, rust-bio provides
 //!
@@ -17,39 +18,139 @@
 //! * a convenient alphabet implementation,
 //! * pairwise alignment,
 //! * suffix arrays,
-//! * BWT and FM-Index,
+//! * the [Burrows-Wheeler-transform (BWT)]()
+//! * the [Full-text index in Minute space index (FM-index)](https://doi.org/10.1109/SFCS.2000.892127),
 //! * FMD-Index for finding supermaximal exact matches,
 //! * a q-gram index,
-//! * an orf research algorithm,
+//! * utilities to work with [PSSMs](https://en.wikipedia.org/wiki/Position_weight_matrix),
+//! * an open reading frame (ORF) search algorithm,
 //! * a rank/select data structure,
-//! * FASTQ and FASTA and BED readers and writers,
+//! * [serde](https://github.com/serde-rs/serde) support for all data structures when built with `nightly` feature,
+//! * readers and writers for FASTQ, FASTA and BED,
 //! * helper functions for combinatorics and dealing with log probabilities,
-//! * an implementation of Hidden Markov Model and related algorithms.
+//! * an implementation of the Hidden Markov Model and related algorithms.
 //!
-//! # Example
+//! For reading and writing SAM/BAM/CRAM, VCF/BCF files or tabix indexed files, have a look at [rust-htslib](https://docs.rs/rust-htslib).
+//!
+//! # Getting started
+//!
+//! We explain how to use Rust-Bio step-by-step.
+//! Users who already have experience with Rust can skip right to [Step 3: Use Rust-Bio in your project](https://docs.rs/bio/#step-3-use-rust-bio-in-your-project).
+//! Users who already know `rust-bio` might want to jump right into the [modules docs](https://docs.rs/bio/#modules)
+//!
+//! ## Step 1: Setting up Rust
+//!
+//! Rust can be installed following the instruction for [rustup](https://rustup.rs/).
+//!
+//!
+//! ## Step 2: Setting up a new Rust project
+//!
+//! Since Rust-Bio is a library, you need to setup your own new Rust project to use Rust-Bio.
+//! With Rust, projects and their dependencies are managed with the builtin package manager [Cargo](https://doc.rust-lang.org/cargo/index.html).
+//! To create a new Rust project, issue
+//!
+//! ```bash
+//! cargo new hello_world --bin
+//! cd hello_world
+//! ```
+//! in your terminal. The flag `--bin` tells Cargo to create an executable project instead of a library.
+//! In [this section](https://doc.rust-lang.org/nightly/book/hello-cargo.html#a-new-project) of the Rust docs, you find details about what Cargo just created for you.
+//!
+//! Your new project can be compiled with
+//! ```bash
+//! cargo build
+//! ```
+//! If dependencies in your project are out of date, update with
+//! ```bash
+//! cargo update
+//! ```
+//! Execute the compiled code with
+//! ```bash
+//! cargo run
+//! ```
+//! If you are new to Rust, we suggest to proceed with [learning Rust](https://www.rust-lang.org/learn) via the Rust docs.
+//!
+//! ## Step 3: Use Rust-Bio in your project
+//!
+//! To use Rust-Bio in your Rust project, add the following to your `Cargo.toml`
+//!
+//! ```toml
+//! [dependencies]
+//! bio = "*"
+//! ```
+//!
+//! and import the crate from your source code:
 //!
 //! ```rust
+//! extern crate bio;
+//! ```
+//!
+//! ## Example: FM-index and FASTQ
+//!
+//! An example of using `rust-bio`:
+//!
+//! ```rust
+//! // Import some modules
+//! use std::io;
 //! use bio::alphabets;
 //! use bio::data_structures::suffix_array::suffix_array;
 //! use bio::data_structures::bwt::{bwt, less, Occ};
 //! use bio::data_structures::fmindex::{FMIndex, FMIndexable};
+//! use bio::io::fastq;
 //!
-//! let text = b"ACGGATGCTGGATCGGATCGCGCTAGCTA$";
-//! let pattern = b"ACCG";
 //!
-//! // Create an FM-Index for a given text.
+//! // a given text
+//! let text = b"ACAGCTCGATCGGTA";
+//! let pattern = b"ATCG";
+//!
+//! // Create an FM-Index for the given text.
+//!
+//! // instantiate an alphabet
 //! let alphabet = alphabets::dna::iupac_alphabet();
+//! // calculate a suffix array
 //! let sa = suffix_array(text);
+//! // calculate the Burrows-Wheeler-transform
 //! let bwt = bwt(text, &sa);
+//! // calculate the vectors less and Occ (occurrences)
 //! let less = less(&bwt, &alphabet);
 //! let occ = Occ::new(&bwt, 3, &alphabet);
+//! // set up FMIndex
 //! let fmindex = FMIndex::new(&bwt, &less, &occ);
-//!
+//! // do a backwards search for the pattern
 //! let interval = fmindex.backward_search(pattern.iter());
 //! let positions = interval.occ(&sa);
+//!
+//! // Iterate over a FASTQ file, use the alphabet to validate read
+//! // sequences and search for exact matches in the FM-Index.
+//!
+//! // create FASTQ reader
+//! let mut reader = fastq::Reader::new(io::stdin());
+//! let mut record = fastq::Record::new();
+//! loop{
+//!   // obtain record or fail with error
+//!   reader.read(&mut record).expect("Failed to parse record");
+//!   if record.is_empty() {
+//!     // out of records
+//!     break;
+//!   }
+//!   let check = record.check();
+//!   if check.is_err() {
+//!       panic!("I got a rubbish record!")
+//!   }
+//!   // obtain sequence
+//!   let seq = record.seq();
+//!   // check, whether seq is in the expected alphabet
+//!   if alphabet.is_word(seq) {
+//!     let interval = fmindex.backward_search(seq.iter());
+//!     let positions = interval.occ(&pos);
+//!   }
+//! }
 //! ```
 //!
-//! # Multithreaded Example
+//! Documentation and further examples for each module can be found in the module descriptions below.
+//!
+//!
+//! ## Example: Multithreaded
 //!
 //! ```rust
 //! use bio::alphabets;
@@ -85,6 +186,24 @@
 //! ```
 //!
 //! Documentation and further examples for each module can be found in the module descriptions below.
+//!
+//! # Benchmarks
+//!
+//! Since Rust-Bio is based on a compiled language, similar performance to C/C++ based libraries can be expected. Indeed, we find the pattern matching algorithms of Rust-Bio to perform in the range of the C++ library Seqan:
+//!
+//! | Algorithm | Rust-Bio | Seqan   |
+//! | --------- | -------: | ------: |
+//! | BNDM      | 77ms     | 80ms    |
+//! | Horspool  | 122ms    | 125ms   |
+//! | BOM       | 103ms    | 107ms   |
+//! | Shift-And | 241ms    | 545ms   |
+//!
+//! We measured 10000 iterations of searching pattern `GCGCGTACACACCGCCCG` in the sequence of the hg38 MT chromosome.
+//! Initialization time of each algorithm for the given pattern was included in each iteration. Benchmarks were conducted with *Cargo bench* for Rust-Bio and *Python timeit* for Seqan on an Intel Core i5-3427U CPU.
+//! Benchmarking Seqan from *Python timeit* entails an overhead of 1.46ms for calling a C++ binary. This overhead was subtracted from above Seqan run times.
+//! Note that this benchmark only compares the two libraries to exemplify that Rust-Bio has comparable speed to C++ libraries: all used algorithms have their advantages for specific text and pattern structures and lengths (see [the pattern matching section in the documentation](https://docs.rs/bio/0.28.2/bio/pattern_matching/index.html))./!
+//!
+
 
 #[macro_use]
 extern crate approx;
