1ff5e369504627d2c2dd97f1e1b0656e555cfa41
Henning Timm <henning.timm@tu-dortmund.de>
2020-07-07 12:32:39 +0200

    Added doctests for dna alphabet functions.

--- src/alphabets/dna.rs
+++ src/alphabets/dna.rs
@@ -49,11 +49,33 @@ lazy_static! {
 }
 
 /// Return complement of given DNA alphabet character (IUPAC alphabet supported).
+///
+/// Casing of input character is preserved, e.g. `t` → `a`, but `T` → `A`.
+/// All `N`s remain as they are.
+///
+/// ```
+/// use bio::alphabets::dna::complement;
+/// assert_eq!(complement(65), 84);   // A → T
+/// assert_eq!(complement(99), 103);  // c → g
+/// assert_eq!(complement(78), 78);   // N → N
+/// assert_eq!(complement(89), 82);   // Y → R
+/// assert_eq!(complement(115), 115); // s → s
+/// ```
 pub fn complement(a: u8) -> u8 {
     COMPLEMENT[a as usize]
 }
 
 /// Calculate reverse complement of given text (IUPAC alphabet supported).
+///
+/// Casing of characters is preserved, e.g. `b"NaCgT"` → `b"aCgTN"`.
+/// All `N`s remain as they are.
+///
+/// ```
+/// use bio::alphabets::dna::revcomp;
+/// assert_eq!(revcomp(b"ACGTN"), b"NACGT");
+/// assert_eq!(revcomp(b"GaTtaCA"), b"TGtaAtC");
+/// assert_eq!(revcomp(b"AGCTYRWSKMDVHBN"), b"NVDBHKMSWYRAGCT");
+/// ```
 pub fn revcomp<C, T>(text: T) -> Vec<u8>
 where
     C: Borrow<u8>,
