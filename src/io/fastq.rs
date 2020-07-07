a064cafc56096226a97f2eecb5411a02c3ba0333
TomKellyGenetics <tomkellygenetics@gmail.com>
2020-07-07 21:01:28 +0900

    remove whitespace

--- src/io/fastq.rs
+++ src/io/fastq.rs
@@ -10,7 +10,7 @@
 //! ```
 //! use std::io;
 //! use bio::io::fastq;
-//! fn main(){  
+//! fn main(){
 //!     let mut reader = fastq::Reader::new(io::stdin());
 //!     let mut record = fastq::Record::new();
 //!     reader.read(&mut record).expect("Failed to parse record");
