5155df177424f5367b346d12efcda916e26011f5
Tianyi Shi <ShiTianyi2001@outlook.com>
2020-09-09 11:42:33 +0800

    use primitive array

--- src/scores/mod.rs
+++ src/scores/mod.rs
@@ -3,6 +3,7 @@
 // This file may not be copied, modified, or distributed
 // except according to those terms.
 
+/// Substitution matrices taken from [seqAn](https://github.com/seqan/seqan/blob/master/include%2Fseqan%2Fscore%2)
 pub use self::blosum62::blosum62;
 pub use self::pam120::pam120;
 pub use self::pam200::pam200;
@@ -10,8 +11,26 @@ pub use self::pam250::pam250;
 pub use self::pam40::pam40;
 
 pub mod blosum62;
-pub mod blosum62_array;
 pub mod pam120;
 pub mod pam200;
 pub mod pam250;
 pub mod pam40;
+
+#[inline]
+fn lookup(a: u8) -> usize {
+    if a == b'*' {
+        26 as usize
+    } else {
+        (a - 65) as usize
+    }
+}
+
+fn lookup_with_gap(gap_char: u8) -> impl Fn(u8) -> usize {
+    move |a| {
+        if a == gap_char {
+            26 as usize
+        } else {
+            (a - 65) as usize
+        }
+    }
+}
