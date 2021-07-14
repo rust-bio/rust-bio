bffce7fcfb466f38dff4a1c00c003ff19a8f899d
Lee Katz <lskatz@gmail.com>
2021-07-13 21:36:55 -0400

    cargo fmt

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -29,25 +29,21 @@ impl Record {
 
     /// Return a fasta::Record object of this generic object
     pub fn fasta_record(&self) -> fasta::Record {
-        let desc_str:&str = self.desc.as_ref().unwrap();
-        let desc_option:std::option::Option<_> = Some(desc_str);
-        let seq:&[u8] = self.seq.as_ref();
+        let desc_str: &str = self.desc.as_ref().unwrap();
+        let desc_option: std::option::Option<_> = Some(desc_str);
+        let seq: &[u8] = self.seq.as_ref();
 
-        fasta::Record::with_attrs(
-            &self.id, desc_option, seq,
-        )
+        fasta::Record::with_attrs(&self.id, desc_option, seq)
     }
 
     /// Return a fastq::Record object of this generic object
     pub fn fastq_record(&self) -> fastq::Record {
-        let desc_str:&str = self.desc.as_ref().unwrap();
-        let desc_option:std::option::Option<_> = Some(desc_str);
-        let seq:&[u8]  = self.seq.as_ref();
-        let qual:&[u8] = self.qual.as_ref();
+        let desc_str: &str = self.desc.as_ref().unwrap();
+        let desc_option: std::option::Option<_> = Some(desc_str);
+        let seq: &[u8] = self.seq.as_ref();
+        let qual: &[u8] = self.qual.as_ref();
 
-        fastq::Record::with_attrs(
-            &self.id, desc_option, seq, qual
-        )
+        fastq::Record::with_attrs(&self.id, desc_option, seq, qual)
     }
 }
 
@@ -55,25 +51,22 @@ impl Record {
 impl From<fastq::Record> for Record {
     fn from(fastq: fastq::Record) -> Self {
         Record {
-            id:   fastq.id().to_string(),
+            id: fastq.id().to_string(),
             desc: Some(fastq.desc().unwrap().to_string()),
-            seq:  std::str::from_utf8(fastq.seq()).unwrap().to_string(),
-            qual: std::str::from_utf8(fastq.qual()).unwrap().to_string()
+            seq: std::str::from_utf8(fastq.seq()).unwrap().to_string(),
+            qual: std::str::from_utf8(fastq.qual()).unwrap().to_string(),
         }
     }
 }
-    
 
 /// Create a generic record from a fasta record
 impl From<fasta::Record> for Record {
     fn from(fasta: fasta::Record) -> Self {
         Record {
-            id:   fasta.id().to_string(),
+            id: fasta.id().to_string(),
             desc: Some(fasta.desc().unwrap().to_string()),
-            seq:  std::str::from_utf8(fasta.seq()).unwrap().to_string(),
+            seq: std::str::from_utf8(fasta.seq()).unwrap().to_string(),
             qual: String::new(),
         }
     }
 }
-    
-
