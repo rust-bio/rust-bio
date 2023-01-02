1bc609ee07708ad91567d19e4a2abfae18cc22c1
Khang Nguyen Duy <40488299+iceghost@users.noreply.github.com>
2023-01-02 19:08:13 +0700

    Update src/data_structures/bwt.rs
    use semanticscholar link
    
    Co-authored-by: David Laehnemann <david.laehnemann@hhu.de>

--- src/data_structures/bwt.rs
+++ src/data_structures/bwt.rs
@@ -3,7 +3,7 @@
 // This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! The [Burrows-Wheeler-Transform](https://web.archive.org/web/20220121112232/http://www.mit.edu/~6.454/www_fall_2002/bwt/burrowswheeler94.pdf) and related data structures.
+//! The [Burrows-Wheeler-Transform](https://www.semanticscholar.org/paper/A-Block-sorting-Lossless-Data-Compression-Algorithm-Burrows-Wheeler/af56e6d4901dcd0f589bf969e604663d40f1be5d) and related data structures.
 //! The implementation is based on the lecture notes
 //! "Algorithmen auf Sequenzen", Kopczynski, Marschall, Martin and Rahmann, 2008 - 2015.
 
