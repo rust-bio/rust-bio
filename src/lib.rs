7d6ca73fb053bb8fa30f2469c9d8b42b0dad9b5d
Khang Nguyen Duy <40488299+iceghost@users.noreply.github.com>
2023-01-02 19:08:30 +0700

    Update src/lib.rs
    use semanticscholar link
    
    Co-authored-by: David Laehnemann <david.laehnemann@hhu.de>

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@
 //! * a convenient alphabet implementation,
 //! * pairwise alignment,
 //! * suffix arrays,
-//! * the [Burrows-Wheeler-transform (BWT)](https://web.archive.org/web/20220121112232/http://www.mit.edu/~6.454/www_fall_2002/bwt/burrowswheeler94.pdf)
+//! * the [Burrows-Wheeler-transform (BWT)](https://www.semanticscholar.org/paper/A-Block-sorting-Lossless-Data-Compression-Algorithm-Burrows-Wheeler/af56e6d4901dcd0f589bf969e604663d40f1be5d)
 //! * the [Full-text index in Minute space index (FM-index)](https://doi.org/10.1109/SFCS.2000.892127),
 //! * FMD-Index for finding supermaximal exact matches,
 //! * a q-gram index,
