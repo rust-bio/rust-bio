c93e0e469ffb0cf9e5db63387b12cfb94f996d72
carocardozo <ccardozo1234@gmail.com>
2023-05-11 14:03:44 +0200

    Traduction implemented

--- src/blast/mod.rs
+++ src/blast/mod.rs
@@ -4,7 +4,7 @@ use crate::utils::Text;
 //use crate::alphabets;
 
 
-/// In place implementation of scan over a slice.
+/// Transcription of a dna (Text :type) into the rna version
 pub fn transcription(dna_str : Text) -> Text {
     //let alphab = alphabets::dna::alphabet();
     let mut rna_str : Text = vec![];
@@ -17,6 +17,75 @@ pub fn transcription(dna_str : Text) -> Text {
         }
     }
     rna_str
+}
+
+/// Translation of a rna (Text :type) into the aminoacid strand
+pub fn translation(rna_str : Text) -> Text{
+    let mut codo : Text = vec![];
+    let mut protein_str : Text = vec![];
+    for i in &rna_str {
+        if codo.len() < 3 {
+            codo.push(*i);
+        }
+        if codo.len() == 3{
+            match codo[0]{
+                b'C'=> match codo[1]{
+                    b'A'=> match codo[2]{
+                        b'U' | b'C' =>protein_str.push(b'H'),
+                        _=>protein_str.push(b'Q'),
+                    },
+                    b'U'=>protein_str.push(b'L'),
+                    b'C'=>protein_str.push(b'P'),
+                    _=>protein_str.push(b'R'), //G
+                },
+                b'G'=> match codo[1]{
+                    b'A'=> match codo[2]{
+                        b'U' | b'C' =>protein_str.push(b'D'),
+                        _=>protein_str.push(b'E'),
+                    },
+                    b'U'=>protein_str.push(b'V'),
+                    b'C'=>protein_str.push(b'A'),
+                    _=>protein_str.push(b'G'), //G
+                },
+                b'A'=> match codo[1]{
+                    b'A'=> match codo[2]{
+                        b'G' | b'A' =>protein_str.push(b'K'),
+                        _=>protein_str.push(b'N'),
+                    },
+                    b'G'=> match codo[2]{
+                        b'G' | b'A' =>protein_str.push(b'R'),
+                        _=>protein_str.push(b'S'),
+                    },
+                    b'U'=>match codo[2]{
+                        b'G' =>protein_str.push(b'M'),
+                        _=>protein_str.push(b'I'),
+                    },
+                    _=>protein_str.push(b'P'), //C
+                },
+                b'U'=> match codo[1]{
+                    b'U'=> match codo[2]{
+                        b'U' | b'C' =>protein_str.push(b'F'),
+                        _=>protein_str.push(b'L'),
+                    },
+                    b'A'=> match codo[2]{
+                        b'U' | b'C' =>protein_str.push(b'Y'),
+                        _=>protein_str.push(b'O'),
+                    },
+                    b'G'=> match codo[2]{
+                        b'U' | b'C' =>protein_str.push(b'C'),
+                        b'G'=>protein_str.push(b'W'),
+                        _=>protein_str.push(b'O'),
+                    },
+                    _=>protein_str.push(b'S'), //C
+                },
+                _=>protein_str.push(b'X'),
+            }
+
+            codo.clear();
+        }
+    }
+    
+    protein_str
 }
 
 
@@ -34,7 +103,7 @@ mod tests {
     }
 
     #[test]
-    fn test_1() {
+    fn printing_strand() {
         let seq_ex = b"ACGT";
         let seq_vec = &vec![b'A',b'C'];
         print_sequence(seq_ex.iter().step_by(1));
@@ -42,9 +111,16 @@ mod tests {
     }
 
     #[test]
-    fn test_2(){
+    fn transcript_simple_dna(){
         let seq_vec : Text= vec![b'A',b'C',b'T'];
         let new_arn = transcription(seq_vec);
         assert_eq!(new_arn,vec![b'A',b'C',b'U']);
+    }
+
+    #[test]
+    fn traduction_one_codon_protein(){
+        let seq_arn : Text = vec![b'U',b'U',b'U'];
+        let new_prot : Text = translation(seq_arn);
+        assert_eq!(new_prot,vec![b'F']);
     }
 }
