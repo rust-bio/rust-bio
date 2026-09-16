8747cebc729d0961fad8e4e3ffd840ed2de2e28f
Johannes Koester <johannes.koester@uni-due.de>
2026-09-16 11:05:56 +0200

    fmt

--- src/pattern_matching/ukkonen.rs
+++ src/pattern_matching/ukkonen.rs
@@ -28,7 +28,7 @@
 //! let pattern = b"TGAGCGA";
 //! let occ: Vec<(usize, usize)> = ukkonen.find_all_end(pattern, text, 1).collect();
 //! assert_eq!(occ, [(13, 1), (14, 1)]);
-//! 
+//!
 //! ukkonen.allow_swaps(true);
 //! ukkonen.allow_indels(false);
 //! let pattern = b"ABCD";
